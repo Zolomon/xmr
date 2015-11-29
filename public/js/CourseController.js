1c7d9bbd5b222558d30f86b12f7ab25109f519d9
Bengt Ericsson <zolomon@gmail.com>
2015-11-29 10:08:23 +0100

    :chestnut: Cleaned up UI

--- public/js/CourseController.js
+++ public/js/CourseController.js
@@ -2,11 +2,27 @@
 (function() {
     var app = angular.module('xmr');
 
+    app
+        .filter('numberFixedLen', function () {
+            return function (n, len) {
+                var num = parseInt(n, 10);
+                len = parseInt(len, 10);
+                if (isNaN(num) || isNaN(len)) {
+                    return n;
+                }
+                num = ''+num;
+                while (num.length < len) {
+                    num = '0'+num;
+                }
+                return num;
+            };
+        });
+    
     var CourseController = ($scope, $routeParams, xmr) => {
         var onCourses = data => $scope.courses = data;       
 
         $scope.toDate = date => xmr.toDate(date);            
-                
+        
         var onCourse = data => {
             $scope.course = data;
 
