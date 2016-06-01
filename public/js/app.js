5a4cb921706ec9f00c7dd97bdcdec3f34537f720
Bengt Ericsson <zolomon@gmail.com>
2016-06-01 11:15:23 +0200

    :coffee: Fix breadcrumb

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,31 @@
 /*global angular*/
 (function() {
-    var xmr = angular.module('xmr', ['ngRoute', 'angular.filter']);
+  var xmr = angular.module('xmr', ['ngRoute', 'angular.filter']);
 
-    xmr.config(function ($routeProvider) {
-        $routeProvider
-            .when('/courses', {
-                templateUrl: 'js/views/courses.html',
-                controller: 'CourseController'
-            })
-            .when('/courses/:id', {
-                templateUrl: 'js/views/course.html',
-                controller: 'CourseController'
-            })
-            .when('/courses/:course_id/exams/:exam_id', {
-                templateUrl: 'js/views/exam.html',
-                controller: 'ExamController'
-            })
-            .when('/tags/:tag_slug', {
-                templateUrl: 'js/views/tag.html',
-                controller: 'TagController'
-            })
-            .when('/courses/:course_id/exams/:exam_id/problems/:problem_id', {
-                templateUrl: 'js/views/problem.html',
-                controller: 'ProblemController'
-            })
-            .otherwise({
-                redirectTo: '/courses'
-            });
-    });
+  xmr.config(function ($routeProvider) {
+    $routeProvider
+      .when('/courses', {
+        templateUrl: 'js/views/courses.html',
+        controller: 'CourseController'
+      })
+      .when('/courses/:id', {
+        templateUrl: 'js/views/course.html',
+        controller: 'CourseController'
+      })
+      .when('/courses/:course_id/exams/:exam_id', {
+        templateUrl: 'js/views/exam.html',
+        controller: 'ExamController'
+      })
+      .when('/tags/:tag_slug', {
+        templateUrl: 'js/views/tag.html',
+        controller: 'TagController'
+      })
+      .when('/courses/:course_id/exams/:exam_id/problems/:problem_id', {
+        templateUrl: 'js/views/problem.html',
+        controller: 'ProblemController'
+      })
+      .otherwise({
+        redirectTo: '/courses'
+      });
+  });
 }());
